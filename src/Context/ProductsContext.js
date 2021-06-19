0b7409a32b581f4c78cd362c0cf83033332abfc8
Sandeep Ashok <51964832+sandeepashok@users.noreply.github.com>
2021-06-19 16:25:11 +0530

    refactor: json updated

--- src/Context/ProductsContext.js
+++ src/Context/ProductsContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { products } from "../SampleData";
+import products from "../sampleData.json";
 
 export const ProductContext = createContext();
 
